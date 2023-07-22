b37c8fa89063c8e49a4d39cdfc5f512780dcfff1
Lorenzo Curcio <Lorenzocurcio2@gmail.com>
2023-07-22 22:00:01 +0200

    aaaaa

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>You have been trolled</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <h1>trololololololol</h1>
     </div>
   );
 }
