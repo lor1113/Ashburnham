fcce880c05baa7a3a2f9347ac265b5822798799a
Lorenzo Curcio <Lorenzocurcio2@gmail.com>
2023-07-22 22:09:39 +0200

    aaaaaaa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Under construction. Spinny atom go wheee.
         </p>
         <a
           className="App-link"
@@ -15,10 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git Gud
         </a>
       </header>
-      <h1>trololololololol</h1>
     </div>
   );
 }
